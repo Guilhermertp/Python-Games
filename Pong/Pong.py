9cf8588bcbd4362e89a351508cb77def4bf3d140
Guilherme Pereira <gr.pereira@campus.fct.unl.pt>
2023-08-13 23:33:21 +0100

    ball movement

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -17,6 +17,12 @@ ball = pygame.Rect(screen_width/2- 15,screen_height/2 -15,30,30)#the subraction 
 player = pygame.Rect(screen_width - 20,screen_height/2 -70 ,10,140) #70 is half of the lenght 140
 opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 
+bg_color = pygame.Color('grey12')
+light_grey = (200,200,200)
+
+ball_speed_x = 7
+ball_speed_y = 7
+
 while True:
     #Handling input
     for event in pygame.event.get(): # check for all the user actions like pressing a key
@@ -24,10 +30,30 @@ while True:
             pygame.quit() #method that closes is the opposite of pygame.init() that starts all the pygame methods (starts the game)
             sys.exit() #makes sure that the program is not still runing in the back
 
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
+    if ball. left <=0 or ball.right >= screen_width:
+        ball_speed_x *= -1
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+
+    #Visuals
+    screen.fill(bg_color)#add the background color by filling the entire display surface
+    pygame.draw.rect(screen,light_grey,player)
+    pygame.draw.rect(screen,light_grey,opponent)
+    pygame.draw.ellipse(screen,light_grey,ball)
+    pygame.draw.aaline(screen, light_grey, (screen_width/2,0),(screen_width/2,screen_height))#line that separates both sides
+#Atention to the order in the loop, because the elements will be drawn on top of each other, by example screen.fill(bg_color) in the end will give an output of only a background
+    # try to remove the comment to check it his code runs in the end screen.fill(bg_color)
+
     # Updating the window
     pygame.display.flip() # thakes everything that comes from the loop and draws in the screen
     clock.tick(60) #limits how fast the loop runs
 
 
-#I'm at 10.32 minutes
+#I'm at 18 minutes
 #https://www.youtube.com/watch?v=Qf3-aDXG8q4&list=PL8ui5HK3oSiEk9HaKoVPxSZA03rmr9Z0k
